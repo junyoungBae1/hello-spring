6dbb782bdfa1ae89d51b1a093fdbc9d77afaf962
Bae <bjy556@gmail.com>
2023-01-08 17:49:47 +0900

    DB, Test

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,35 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     // memberService를 스프링 빈으로 등록
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository()); // 스프링 빈에 등록되어 있는 MemberRepository를 MemberService에 넣어줌
+        // 스프링 빈에 등록되어 있는 MemberRepository를 MemberService에 넣어줌
+        return new MemberService(memberRepository());
     }
 
     // memberRepository를 스프링 빈으로 등록
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +19,29 @@ public class MemberController {
     @Autowired   //스프링이 스프링 컨테이너에 있는 MemberService를 가져다 연결을 시켜줌(Controller랑 Service를 연결시켜줌, dependency injection)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    /*
+    회원 등록 폼 컨트롤러 추가
+    */
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) { // 등록을 누르면 MemberFrom의 객체에 setName()함수로 전달된 name 값이 들어감
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 홈 화면으로 돌아감
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
